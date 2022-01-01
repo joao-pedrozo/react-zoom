9775489212b7bcb615c38240df08de9647ddeb95
João Pedrozo <jp.pedrozo@hotmail.com>
2021-12-31 22:59:34 -0300

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: [
+    "../src/components/**/stories.tsx",
+    "../src/components/**/**/stories.tsx",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
