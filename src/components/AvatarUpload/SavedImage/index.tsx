767182160a6bf09d95bec2737ccc93ebe7388a55
João Pedrozo <jp.pedrozo@hotmail.com>
2022-01-02 16:41:06 -0300

    fix styles

--- src/components/AvatarUpload/SavedImage/index.tsx
+++ src/components/AvatarUpload/SavedImage/index.tsx
@@ -1,37 +1,35 @@
-import React, { ChangeEventHandler, RefObject, useEffect, useRef } from "react";
-import styled from "styled-components";
-
-import Image from "../../Image";
-import LogoInteractInfo from "../LogoInteractInfo";
+import React, { RefObject, useEffect, useRef } from 'react';
+import Image from '../../Image';
+import LogoInteractInfo from '../LogoInteractInfo';
 
 interface Props {
-  imageFile: File | null;
-  zoomLevel: number;
-  inputRef: RefObject<HTMLInputElement>;
-  onChangeInput: (ev: React.FormEvent<HTMLInputElement>) => void;
+	imageFile: File | null;
+	zoomLevel: number;
+	inputRef: RefObject<HTMLInputElement>;
+	onChangeInput: (ev: React.FormEvent<HTMLInputElement>) => void;
 }
 
 const SavedImage = ({
-  imageFile,
-  zoomLevel,
-  inputRef,
-  onChangeInput,
+	imageFile,
+	zoomLevel,
+	inputRef,
+	onChangeInput,
 }: Props) => {
-  const imgRef = useRef<HTMLDivElement>(null);
+	const imgRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    imgRef.current?.style.setProperty(
-      "transform",
-      `scale(${1 + zoomLevel / 10})`
-    );
-  }, []);
+	useEffect(() => {
+		imgRef.current?.style.setProperty(
+			'transform',
+			`scale(${1 + zoomLevel / 10})`
+		);
+	}, []);
 
-  return (
-    <div className="content-wrapper">
-      <Image file={imageFile} imgRef={imgRef} />
-      <LogoInteractInfo inputRef={inputRef} onChangeInput={onChangeInput} />
-    </div>
-  );
+	return (
+		<div className='content-wrapper'>
+			<Image file={imageFile} imgRef={imgRef} />
+			<LogoInteractInfo inputRef={inputRef} onChangeInput={onChangeInput} />
+		</div>
+	);
 };
 
 export default SavedImage;
